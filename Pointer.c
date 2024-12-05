f5deacf54089fa66d02342efd8185fc611b30efe
vvvvo0 <idvvvvo@gmail.com>
2024-12-05 22:58:19 +0900

    초기 커밋.

---
+++ Pointer.c
@@ -0,0 +1,11 @@
+#define  _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+{
+	int arr1[3] = { 0,1,2 };
+	printf("배열의 이름: %p\n", arr);
+	printf("첫 번째 요소: %p\n", &arr1[0]);
+	printf("두 번째 요소: %p\n", &arr1[1]);
+	printf("세 번째 요소: %p\n", &arr1[2]);
+
+}
